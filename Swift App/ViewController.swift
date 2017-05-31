aa6c6f336dc1d6ac44dc3440ce0f823887febae7
moayyd <m02yyd@gmail.com>
2017-05-31 14:18:19 +0300

    Change to 20

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
     @IBAction func buttonTapped(_ sender: Any) {
         tapCount = tapCount + 1
         print(tapCount)
-        if tapCount >= 10 {
-            coolLabel.text = "You tapped the button 10 times"
+        if tapCount >= 20 {
+            coolLabel.text = "You tapped the button 20 times"
         }
     }
     override func viewDidLoad() {
